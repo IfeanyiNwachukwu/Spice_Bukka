0170b983fae0da15169e5243bde0c4121e46a134
Ifeanyi Nwachukwu <ifenwachukwu7@outlook.com>
2023-03-15 02:17:03 +0100

    Completed Coupon section

--- SpicyBukka/SpicyBukka/Data/ApplicationDbContext.cs
+++ SpicyBukka/SpicyBukka/Data/ApplicationDbContext.cs
@@ -16,5 +16,7 @@ namespace SpicyBukka.Data
         public DbSet<SubCategory> SubCategory { get; set; }
 
         public DbSet<MenuItem> MenuItem { get; set; }
+
+        public DbSet<Coupon> Coupon { get; set; }
     }
 }
