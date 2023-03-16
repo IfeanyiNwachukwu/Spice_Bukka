15d1f9561f9c6644a8f7fedb68e95e6e18ba98b5
Ifeanyi Nwachukwu <ifenwachukwu7@outlook.com>
2023-03-16 04:02:17 +0100

    Completed landing page

--- SpicyBukka/SpicyBukka/Areas/Customer/Controllers/HomeController.cs
+++ SpicyBukka/SpicyBukka/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using SpicyBukka.Data;
 using SpicyBukka.Models;
+using SpicyBukka.Models.ViewModels;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SpicyBukka.Areas.Customer.Controllers
 {
@@ -9,15 +14,23 @@ namespace SpicyBukka.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IndexViewModel IndexVM = new IndexViewModel()
+            {
+                MenuItem = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync(),
+                Category = await _db.Category.ToListAsync(),
+                Coupon = await _db.Coupon.Where(c => c.IsActive == true).ToListAsync()
+            };
+            return View(IndexVM);
         }
 
         public IActionResult Privacy()
---
+++ SpicyBukka/SpicyBukka/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SpicyBukka.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<MenuItem> MenuItem { get; set; }
+
+        public IEnumerable<Category> Category { get; set; }
+
+        public IEnumerable<Coupon> Coupon { get; set; }
+
+
+    }
+}
